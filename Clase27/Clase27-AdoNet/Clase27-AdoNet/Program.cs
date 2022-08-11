a7c000bf74860e8d6b70708f6c07fc822ccb2815
Gimena Gil Fernández <104610596+GimeGilFer@users.noreply.github.com>
2022-08-10 21:01:49 -0300

    Add files via upload

---
+++ Clase27/Clase27-AdoNet/Clase27-AdoNet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+//var connectionString = "Provider=MSOLEDBSQL;Server=myServerName\theInstanceName;Database=myDataBase;Trusted_Connection=yes;";
+var connectionString = @"Server=.\SQL2019;Database=Northwind;Trusted_Connection=yes;";
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+
+    var command = new SqlCommand("SELECT * FROM Customers", connection);
+    var reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        Console.WriteLine($"{reader["CustomerID"]} | {reader["CompanyName"]} | {reader["ContactName"]}");
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Polo Tecnológico Mina Clavero
+## Curso de Introduccion a NET 6.0 / Año 2022
+
+Este repositorio es parte del curso de Introduccion a NET 6.0 dictado por el Polo Tecnologico Mina Clavero.
+En este repositorio se encuentran ejemplos y prácticas de los códigos utilizados en cada una de las clases.
+
+
